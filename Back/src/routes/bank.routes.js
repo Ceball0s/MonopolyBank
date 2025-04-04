e25178a0eacd77355b9d10f0e399228308b22ac5
CodeZercyhX <167056739+CodeZercyhX@users.noreply.github.com>
2025-04-04 10:20:00 -0500

    Update bank.routes.js
    Adición de la ruta para el historial de transacciones

--- Back/src/routes/bank.routes.js
+++ Back/src/routes/bank.routes.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { transferMoney, payRent } = require('../controllers/bank.controller');
+const { transferMoney, getTransactionHistory } = require('../controllers/bank.controller');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/transfer-money', authMiddleware, transferMoney);
+router.get('/history', authMiddleware, getTransactionHistory);
 
 module.exports = router;
