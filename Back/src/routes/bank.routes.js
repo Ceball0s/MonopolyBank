c459ab2253e4bec0ee72a49a1ba5467cf681878c
CodeZercyhX <167056739+CodeZercyhX@users.noreply.github.com>
2025-04-04 15:42:24 -0500

    Update bank.routes.js
    Adición de balances se puede obtener el monto que tiene cada jugador, procediendo a la sala a través del código de la sala

--- Back/src/routes/bank.routes.js
+++ Back/src/routes/bank.routes.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { transferMoney, getTransactionHistory } = require('../controllers/bank.controller');
+const { transferMoney, getTransactionHistory, getBalancesByGame } = require('../controllers/bank.controller');
 const authMiddleware = require('../middleware/authMiddleware');
 
 router.post('/transfer-money', authMiddleware, transferMoney);
 router.get('/history', authMiddleware, getTransactionHistory);
+router.get('/balances/:code', authMiddleware, getBalancesByGame);
 
 module.exports = router;
